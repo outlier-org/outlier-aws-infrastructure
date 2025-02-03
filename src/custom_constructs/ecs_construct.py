fbe53c49da10383dc2d999847e725317099cadfc
Dobson Dunavant <136488074+SavvasDobsonDunavant@users.noreply.github.com>
2025-02-03 17:13:27 -0500

    fix task def setup

--- src/custom_constructs/ecs_construct.py
+++ src/custom_constructs/ecs_construct.py
@@ -1,7 +1,5 @@
-# src/custom_constructs/ecs_construct.py
 from aws_cdk import aws_ecs as ecs
 from aws_cdk import aws_ec2 as ec2
-from aws_cdk import aws_elasticloadbalancingv2 as elbv2
 import aws_cdk as cdk
 from constructs import Construct
 from .base_construct import BaseConstruct
@@ -24,23 +22,33 @@ class EcsConstruct(BaseConstruct):
             vpc=vpc
         )
 
+        # Import existing task definitions
+        service_task_def = ecs.TaskDefinition.from_task_definition_arn(
+            self,
+            "ServiceTaskDef",
+            "arn:aws:ecs:us-east-1:528757783796:task-definition/Outlier-Service-Task-nightly:16"
+        )
+
+        jobs_task_def = ecs.TaskDefinition.from_task_definition_arn(
+            self,
+            "JobsServiceTaskDef",
+            "arn:aws:ecs:us-east-1:528757783796:task-definition/Outlier-job-task-nightly:16"
+        )
+
         # Main Service
         self._service = ecs.FargateService(
             self,
             "Service",
             service_name=f"outlier-service-ecs-{self.environment}-test",
             cluster=self._cluster,
+            task_definition=service_task_def,
             desired_count=2,
             platform_version=ecs.FargatePlatformVersion.VERSION1_4,
             security_groups=security_groups,
-            vpc_subnets=ec2.SubnetSelection(
-                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
-            ),
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
             assign_public_ip=False,
-            health_check_grace_period=cdk.Duration.seconds(0),
-            deployment_controller=ecs.DeploymentController(
-                type=ecs.DeploymentControllerType.CODE_DEPLOY
-            )
+            health_check_grace_period=cdk.Duration.seconds(60),
+            deployment_controller=ecs.DeploymentController(type=ecs.DeploymentControllerType.CODE_DEPLOY)
         )
 
         # Jobs Service
@@ -49,17 +57,14 @@ class EcsConstruct(BaseConstruct):
             "JobsService",
             service_name=f"outlier-job-service-ecs-{self.environment}-test",
             cluster=self._cluster,
+            task_definition=jobs_task_def,
             desired_count=1,
             platform_version=ecs.FargatePlatformVersion.VERSION1_4,
             security_groups=security_groups,
-            vpc_subnets=ec2.SubnetSelection(
-                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
-            ),
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
             assign_public_ip=False,
-            health_check_grace_period=cdk.Duration.seconds(0),
-            deployment_controller=ecs.DeploymentController(
-                type=ecs.DeploymentControllerType.CODE_DEPLOY
-            )
+            health_check_grace_period=cdk.Duration.seconds(60),
+            deployment_controller=ecs.DeploymentController(type=ecs.DeploymentControllerType.CODE_DEPLOY)
         )
 
     @property
