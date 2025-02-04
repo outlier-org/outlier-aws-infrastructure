81c6e1980c6cc16341d9178bd9caae6b33670414
Dobson Dunavant <136488074+SavvasDobsonDunavant@users.noreply.github.com>
2025-02-04 18:17:40 -0500

    Update base_stack.py

--- src/stacks/base_stack.py
+++ src/stacks/base_stack.py
@@ -4,9 +4,9 @@ from constructs import Construct
 from custom_constructs.network_construct import NetworkConstruct
 from custom_constructs.storage_construct import StorageConstruct
 from custom_constructs.iam_construct import IamConstruct
-# from custom_constructs.alb_construct import AlbConstruct
-# from custom_constructs.ecs_construct import EcsConstruct
-# from custom_constructs.pipeline_construct import CodePipelineConstruct
+from custom_constructs.alb_construct import AlbConstruct
+from custom_constructs.ecs_construct import EcsConstruct
+from custom_constructs.pipeline_construct import CodePipelineConstruct
 
 class BaseStack(cdk.Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
@@ -21,42 +21,42 @@ class BaseStack(cdk.Stack):
         # IAM resources
         iam = IamConstruct(self, "IamConstruct")
 
-        # # Create ALB with target groups
-        # alb = AlbConstruct(
-        #     self,
-        #     "AlbConstruct",
-        #     vpc=network.vpc,
-        #     security_group=network.alb_security_group,
-        #     subnets=network.vpc.public_subnets
-        # )
-        #
-        # # Create ECS resources (basic infrastructure only)
-        # ecs = EcsConstruct(
-        #     self,
-        #     "EcsConstruct",
-        #     vpc=network.vpc,
-        #     security_groups=[network.service_security_group],
-        #     execution_role=iam.task_execution_role,
-        #     task_role=iam.task_role,
-        #     service_target_group=alb.service_tg_1,
-        #     jobs_target_group=alb.jobs_tg_1
-        # )
-        #
-        # # Create Pipeline resources (handles deployments and updates)
-        # pipeline = CodePipelineConstruct(
-        #     self,
-        #     "PipelineConstruct",
-        #     ecs_cluster=ecs.cluster,
-        #     ecs_service=ecs.service,
-        #     ecs_jobs_service=ecs.jobs_service,
-        #     prod_listener=alb.production_listener,
-        #     test_listener=alb.test_listener,
-        #     service_target_groups=[
-        #         alb.service_tg_1,
-        #         alb.service_tg_2
-        #     ],
-        #     jobs_target_groups=[
-        #         alb.jobs_tg_1,
-        #         alb.jobs_tg_2
-        #     ]
-        # )
+        # Create ALB with target groups
+        alb = AlbConstruct(
+            self,
+            "AlbConstruct",
+            vpc=network.vpc,
+            security_group=network.alb_security_group,
+            subnets=network.vpc.public_subnets
+        )
+
+        # Create ECS resources (basic infrastructure only)
+        ecs = EcsConstruct(
+            self,
+            "EcsConstruct",
+            vpc=network.vpc,
+            security_groups=[network.service_security_group],
+            execution_role=iam.task_execution_role,
+            task_role=iam.task_role,
+            service_target_group=alb.service_tg_1,
+            jobs_target_group=alb.jobs_tg_1
+        )
+
+        # Create Pipeline resources (handles deployments and updates)
+        pipeline = CodePipelineConstruct(
+            self,
+            "PipelineConstruct",
+            ecs_cluster=ecs.cluster,
+            ecs_service=ecs.service,
+            ecs_jobs_service=ecs.jobs_service,
+            prod_listener=alb.production_listener,
+            test_listener=alb.test_listener,
+            service_target_groups=[
+                alb.service_tg_1,
+                alb.service_tg_2
+            ],
+            jobs_target_groups=[
+                alb.jobs_tg_1,
+                alb.jobs_tg_2
+            ]
+        )
