1d045dad484beb59d6c6c34cd2510a375ad5b076
Dobson Dunavant <136488074+SavvasDobsonDunavant@users.noreply.github.com>
2025-02-05 07:52:23 -0500

    e2e setup test

--- src/custom_constructs/alb_construct.py
+++ src/custom_constructs/alb_construct.py
@@ -1,8 +1,9 @@
 # src/custom_constructs/alb_construct.py
-from aws_cdk import aws_elasticloadbalancingv2 as elbv2
-from aws_cdk import aws_ec2 as ec2
-from aws_cdk import aws_certificatemanager as acm
-import aws_cdk as cdk
+from aws_cdk import (
+    aws_ec2 as ec2,
+    aws_elasticloadbalancingv2 as elbv2,
+    Duration
+)
 from constructs import Construct
 from .base_construct import BaseConstruct
 
@@ -12,223 +13,314 @@ class AlbConstruct(BaseConstruct):
             scope: Construct,
             id: str,
             vpc: ec2.IVpc,
-            security_group: ec2.ISecurityGroup,
-            subnets: list[ec2.ISubnet]
+            security_group: ec2.ISecurityGroup
     ):
         super().__init__(scope, id)
 
-        # Import existing certificate
-        certificate = acm.Certificate.from_certificate_arn(
-            self,
-            "AlbCertificate",
-            "arn:aws:acm:us-east-1:528757783796:certificate/a9387ff3-5327-448e-b1a2-3ca8e3e1e7f4"
-        )
-
         # Create ALB
-        self._load_balancer = elbv2.ApplicationLoadBalancer(
+        self._alb = elbv2.ApplicationLoadBalancer(
             self,
-            "ApplicationLoadBalancer",
-            load_balancer_name=f"outlier-service-alb-{self.environment}-test",
+            "ALB",
             vpc=vpc,
             internet_facing=True,
             security_group=security_group,
-            vpc_subnets=ec2.SubnetSelection(subnets=subnets),
-            idle_timeout=cdk.Duration.seconds(60),
-            http2_enabled=True,
-            deletion_protection=False
+            load_balancer_name=f"outlier-alb-{self.environment}"
         )
 
-        # Create Main Service Target Groups with optimized health checks
-        self._service_tg_1 = elbv2.ApplicationTargetGroup(
+        # Create main service target group
+        self._main_target_group = elbv2.ApplicationTargetGroup(
             self,
-            "ServiceTargetGroup1",
-            target_group_name=f"out-main-tg-3-{self.environment}-test",
+            "MainServiceTargetGroup",
             vpc=vpc,
-            port=1337,
-            protocol=elbv2.ApplicationProtocol.HTTP,
-            target_type=elbv2.TargetType.IP,
-            health_check=elbv2.HealthCheck(
-                path="/",
-                healthy_http_codes="200-499",
-                interval=cdk.Duration.seconds(30),
-                timeout=cdk.Duration.seconds(5),
-                healthy_threshold_count=2,
-                unhealthy_threshold_count=2,
-                port="1337"
-            ),
-            deregistration_delay=cdk.Duration.seconds(30)  # Faster deregistration
-        )
-
-        self._service_tg_2 = elbv2.ApplicationTargetGroup(
-            self,
-            "ServiceTargetGroup2",
-            target_group_name=f"out-main-tg-4-{self.environment}-test",
-            vpc=vpc,
-            port=1337,
-            protocol=elbv2.ApplicationProtocol.HTTP,
-            target_type=elbv2.TargetType.IP,
-            health_check=elbv2.HealthCheck(
-                path="/",
-                healthy_http_codes="200-499",
-                interval=cdk.Duration.seconds(30),
-                timeout=cdk.Duration.seconds(5),
-                healthy_threshold_count=2,
-                unhealthy_threshold_count=2,
-                port="1337"
-            ),
-            deregistration_delay=cdk.Duration.seconds(30)
-        )
-
-        # Create Jobs Service Target Groups with optimized health checks
-        self._jobs_tg_1 = elbv2.ApplicationTargetGroup(
-            self,
-            "JobsTargetGroup1",
-            target_group_name=f"out-jobs-tg-3-{self.environment}-test",
-            vpc=vpc,
-            port=1337,
-            protocol=elbv2.ApplicationProtocol.HTTP,
-            target_type=elbv2.TargetType.IP,
-            health_check=elbv2.HealthCheck(
-                path="/",
-                healthy_http_codes="200-499",
-                interval=cdk.Duration.seconds(30),
-                timeout=cdk.Duration.seconds(5),
-                healthy_threshold_count=2,
-                unhealthy_threshold_count=2,
-                port="1337"
-            ),
-            deregistration_delay=cdk.Duration.seconds(30)
-        )
-
-        self._jobs_tg_2 = elbv2.ApplicationTargetGroup(
-            self,
-            "JobsTargetGroup2",
-            target_group_name=f"out-jobs-tg-4-{self.environment}-test",
-            vpc=vpc,
-            port=1337,
-            protocol=elbv2.ApplicationProtocol.HTTP,
-            target_type=elbv2.TargetType.IP,
-            health_check=elbv2.HealthCheck(
-                path="/",
-                healthy_http_codes="200-499",
-                interval=cdk.Duration.seconds(30),
-                timeout=cdk.Duration.seconds(5),
-                healthy_threshold_count=2,
-                unhealthy_threshold_count=2,
-                port="1337"
-            ),
-            deregistration_delay=cdk.Duration.seconds(30)
-        )
-
-        # Add HTTP Listener (defaults to service tg 1)
-        self._http_listener = self._load_balancer.add_listener(
-            "HttpListener",
             port=80,
-            default_target_groups=[self._service_tg_1]
-        )
-
-        # Add HTTPS Listener (production traffic)
-        self._prod_listener = self._load_balancer.add_listener(
-            "HttpsListener",
-            port=443,
-            certificates=[certificate],
-            default_target_groups=[self._service_tg_1]
-        )
-
-        # Add Test Listener (for blue/green test traffic)
-        self._test_listener = self._load_balancer.add_listener(
-            "TestListener",
-            port=8080,
             protocol=elbv2.ApplicationProtocol.HTTP,
-            default_target_groups=[self._service_tg_1]
+            target_type=elbv2.TargetType.IP,
+            health_check=elbv2.HealthCheck(
+                path="/",
+                healthy_http_codes="200",
+                interval=Duration.seconds(30),
+                timeout=Duration.seconds(5)
+            )
         )
 
-        # Add production listener rules for jobs
-        self._prod_listener.add_target_groups(
-            "CronRule",
+        # Main listener
+        self._main_listener = self._alb.add_listener(
+            "MainListener",
+            port=80,
+            default_target_groups=[self._main_target_group]
+        )
+
+        # Jobs service target group
+        self._jobs_target_group = elbv2.ApplicationTargetGroup(
+            self,
+            "JobsServiceTargetGroup",
+            vpc=vpc,
+            port=80,
+            protocol=elbv2.ApplicationProtocol.HTTP,
+            target_type=elbv2.TargetType.IP,
+            health_check=elbv2.HealthCheck(
+                path="/",
+                healthy_http_codes="200",
+                interval=Duration.seconds(30),
+                timeout=Duration.seconds(5)
+            )
+        )
+
+        # Add jobs listener rule
+        self._main_listener.add_action(
+            "JobsListenerRule",
+            conditions=[
+                elbv2.ListenerCondition.path_patterns(["/jobs/*"])
+            ],
             priority=1,
-            conditions=[
-                elbv2.ListenerCondition.path_patterns([
-                    "/cron-script",
-                    "/cron-script/*",
-                    "/test-cron",
-                    "/test-cron/*"
-                ])
-            ],
-            target_groups=[self._jobs_tg_1]
+            action=elbv2.ListenerAction.forward([self._jobs_target_group])
         )
 
-        self._prod_listener.add_target_groups(
-            "JobsRule",
-            priority=2,
-            conditions=[
-                elbv2.ListenerCondition.path_patterns([
-                    "/proctorio/send-monthly-report",
-                    "/set/active-campaign-tags",
-                    "/sync/airtable-active-campaign",
-                    "/script/*"
-                ])
-            ],
-            target_groups=[self._jobs_tg_1]
-        )
-
-        # Add test listener rules for jobs (mirror production rules)
-        self._test_listener.add_target_groups(
-            "TestCronRule",
-            priority=1,
-            conditions=[
-                elbv2.ListenerCondition.path_patterns([
-                    "/cron-script",
-                    "/cron-script/*",
-                    "/test-cron",
-                    "/test-cron/*"
-                ])
-            ],
-            target_groups=[self._jobs_tg_1]
-        )
-
-        self._test_listener.add_target_groups(
-            "TestJobsRule",
-            priority=2,
-            conditions=[
-                elbv2.ListenerCondition.path_patterns([
-                    "/proctorio/send-monthly-report",
-                    "/set/active-campaign-tags",
-                    "/sync/airtable-active-campaign",
-                    "/script/*"
-                ])
-            ],
-            target_groups=[self._jobs_tg_1]
-        )
-
-        # Add tags
-        cdk.Tags.of(self).add("bounded_context", "outlier")
-        cdk.Tags.of(self).add("env", self.environment)
+    @property
+    def alb(self) -> elbv2.IApplicationLoadBalancer:
+        return self._alb
 
     @property
-    def load_balancer(self) -> elbv2.IApplicationLoadBalancer:
-        return self._load_balancer
+    def main_target_group(self) -> elbv2.IApplicationTargetGroup:
+        return self._main_target_group
 
     @property
-    def production_listener(self) -> elbv2.IApplicationListener:
-        return self._prod_listener
+    def jobs_target_group(self) -> elbv2.IApplicationTargetGroup:
+        return self._jobs_target_group
 
-    @property
-    def test_listener(self) -> elbv2.IApplicationListener:
-        return self._test_listener
 
-    @property
-    def service_tg_1(self) -> elbv2.IApplicationTargetGroup:
-        return self._service_tg_1
 
-    @property
-    def service_tg_2(self) -> elbv2.IApplicationTargetGroup:
-        return self._service_tg_2
-
-    @property
-    def jobs_tg_1(self) -> elbv2.IApplicationTargetGroup:
-        return self._jobs_tg_1
-
-    @property
-    def jobs_tg_2(self) -> elbv2.IApplicationTargetGroup:
-        return self._jobs_tg_2
+# # src/custom_constructs/alb_construct.py
+# from aws_cdk import aws_elasticloadbalancingv2 as elbv2
+# from aws_cdk import aws_ec2 as ec2
+# from aws_cdk import aws_certificatemanager as acm
+# import aws_cdk as cdk
+# from constructs import Construct
+# from .base_construct import BaseConstruct
+#
+# class AlbConstruct(BaseConstruct):
+#     def __init__(
+#             self,
+#             scope: Construct,
+#             id: str,
+#             vpc: ec2.IVpc,
+#             security_group: ec2.ISecurityGroup,
+#             subnets: list[ec2.ISubnet]
+#     ):
+#         super().__init__(scope, id)
+#
+#         # Import existing certificate
+#         certificate = acm.Certificate.from_certificate_arn(
+#             self,
+#             "AlbCertificate",
+#             "arn:aws:acm:us-east-1:528757783796:certificate/a9387ff3-5327-448e-b1a2-3ca8e3e1e7f4"
+#         )
+#
+#         # Create ALB
+#         self._load_balancer = elbv2.ApplicationLoadBalancer(
+#             self,
+#             "ApplicationLoadBalancer",
+#             load_balancer_name=f"outlier-service-alb-{self.environment}-test",
+#             vpc=vpc,
+#             internet_facing=True,
+#             security_group=security_group,
+#             vpc_subnets=ec2.SubnetSelection(subnets=subnets),
+#             idle_timeout=cdk.Duration.seconds(60),
+#             http2_enabled=True,
+#             deletion_protection=False
+#         )
+#
+#         # Create Main Service Target Groups with optimized health checks
+#         self._service_tg_1 = elbv2.ApplicationTargetGroup(
+#             self,
+#             "ServiceTargetGroup1",
+#             target_group_name=f"out-main-tg-3-{self.environment}-test",
+#             vpc=vpc,
+#             port=1337,
+#             protocol=elbv2.ApplicationProtocol.HTTP,
+#             target_type=elbv2.TargetType.IP,
+#             health_check=elbv2.HealthCheck(
+#                 path="/",
+#                 healthy_http_codes="200-499",
+#                 interval=cdk.Duration.seconds(30),
+#                 timeout=cdk.Duration.seconds(5),
+#                 healthy_threshold_count=2,
+#                 unhealthy_threshold_count=2,
+#                 port="1337"
+#             ),
+#             deregistration_delay=cdk.Duration.seconds(30)  # Faster deregistration
+#         )
+#
+#         self._service_tg_2 = elbv2.ApplicationTargetGroup(
+#             self,
+#             "ServiceTargetGroup2",
+#             target_group_name=f"out-main-tg-4-{self.environment}-test",
+#             vpc=vpc,
+#             port=1337,
+#             protocol=elbv2.ApplicationProtocol.HTTP,
+#             target_type=elbv2.TargetType.IP,
+#             health_check=elbv2.HealthCheck(
+#                 path="/",
+#                 healthy_http_codes="200-499",
+#                 interval=cdk.Duration.seconds(30),
+#                 timeout=cdk.Duration.seconds(5),
+#                 healthy_threshold_count=2,
+#                 unhealthy_threshold_count=2,
+#                 port="1337"
+#             ),
+#             deregistration_delay=cdk.Duration.seconds(30)
+#         )
+#
+#         # Create Jobs Service Target Groups with optimized health checks
+#         self._jobs_tg_1 = elbv2.ApplicationTargetGroup(
+#             self,
+#             "JobsTargetGroup1",
+#             target_group_name=f"out-jobs-tg-3-{self.environment}-test",
+#             vpc=vpc,
+#             port=1337,
+#             protocol=elbv2.ApplicationProtocol.HTTP,
+#             target_type=elbv2.TargetType.IP,
+#             health_check=elbv2.HealthCheck(
+#                 path="/",
+#                 healthy_http_codes="200-499",
+#                 interval=cdk.Duration.seconds(30),
+#                 timeout=cdk.Duration.seconds(5),
+#                 healthy_threshold_count=2,
+#                 unhealthy_threshold_count=2,
+#                 port="1337"
+#             ),
+#             deregistration_delay=cdk.Duration.seconds(30)
+#         )
+#
+#         self._jobs_tg_2 = elbv2.ApplicationTargetGroup(
+#             self,
+#             "JobsTargetGroup2",
+#             target_group_name=f"out-jobs-tg-4-{self.environment}-test",
+#             vpc=vpc,
+#             port=1337,
+#             protocol=elbv2.ApplicationProtocol.HTTP,
+#             target_type=elbv2.TargetType.IP,
+#             health_check=elbv2.HealthCheck(
+#                 path="/",
+#                 healthy_http_codes="200-499",
+#                 interval=cdk.Duration.seconds(30),
+#                 timeout=cdk.Duration.seconds(5),
+#                 healthy_threshold_count=2,
+#                 unhealthy_threshold_count=2,
+#                 port="1337"
+#             ),
+#             deregistration_delay=cdk.Duration.seconds(30)
+#         )
+#
+#         # Add HTTP Listener (defaults to service tg 1)
+#         self._http_listener = self._load_balancer.add_listener(
+#             "HttpListener",
+#             port=80,
+#             default_target_groups=[self._service_tg_1]
+#         )
+#
+#         # Add HTTPS Listener (production traffic)
+#         self._prod_listener = self._load_balancer.add_listener(
+#             "HttpsListener",
+#             port=443,
+#             certificates=[certificate],
+#             default_target_groups=[self._service_tg_1]
+#         )
+#
+#         # Add Test Listener (for blue/green test traffic)
+#         self._test_listener = self._load_balancer.add_listener(
+#             "TestListener",
+#             port=8080,
+#             protocol=elbv2.ApplicationProtocol.HTTP,
+#             default_target_groups=[self._service_tg_1]
+#         )
+#
+#         # Add production listener rules for jobs
+#         self._prod_listener.add_target_groups(
+#             "CronRule",
+#             priority=1,
+#             conditions=[
+#                 elbv2.ListenerCondition.path_patterns([
+#                     "/cron-script",
+#                     "/cron-script/*",
+#                     "/test-cron",
+#                     "/test-cron/*"
+#                 ])
+#             ],
+#             target_groups=[self._jobs_tg_1]
+#         )
+#
+#         self._prod_listener.add_target_groups(
+#             "JobsRule",
+#             priority=2,
+#             conditions=[
+#                 elbv2.ListenerCondition.path_patterns([
+#                     "/proctorio/send-monthly-report",
+#                     "/set/active-campaign-tags",
+#                     "/sync/airtable-active-campaign",
+#                     "/script/*"
+#                 ])
+#             ],
+#             target_groups=[self._jobs_tg_1]
+#         )
+#
+#         # Add test listener rules for jobs (mirror production rules)
+#         self._test_listener.add_target_groups(
+#             "TestCronRule",
+#             priority=1,
+#             conditions=[
+#                 elbv2.ListenerCondition.path_patterns([
+#                     "/cron-script",
+#                     "/cron-script/*",
+#                     "/test-cron",
+#                     "/test-cron/*"
+#                 ])
+#             ],
+#             target_groups=[self._jobs_tg_1]
+#         )
+#
+#         self._test_listener.add_target_groups(
+#             "TestJobsRule",
+#             priority=2,
+#             conditions=[
+#                 elbv2.ListenerCondition.path_patterns([
+#                     "/proctorio/send-monthly-report",
+#                     "/set/active-campaign-tags",
+#                     "/sync/airtable-active-campaign",
+#                     "/script/*"
+#                 ])
+#             ],
+#             target_groups=[self._jobs_tg_1]
+#         )
+#
+#         # Add tags
+#         cdk.Tags.of(self).add("bounded_context", "outlier")
+#         cdk.Tags.of(self).add("env", self.environment)
+#
+#     @property
+#     def load_balancer(self) -> elbv2.IApplicationLoadBalancer:
+#         return self._load_balancer
+#
+#     @property
+#     def production_listener(self) -> elbv2.IApplicationListener:
+#         return self._prod_listener
+#
+#     @property
+#     def test_listener(self) -> elbv2.IApplicationListener:
+#         return self._test_listener
+#
+#     @property
+#     def service_tg_1(self) -> elbv2.IApplicationTargetGroup:
+#         return self._service_tg_1
+#
+#     @property
+#     def service_tg_2(self) -> elbv2.IApplicationTargetGroup:
+#         return self._service_tg_2
+#
+#     @property
+#     def jobs_tg_1(self) -> elbv2.IApplicationTargetGroup:
+#         return self._jobs_tg_1
+#
+#     @property
+#     def jobs_tg_2(self) -> elbv2.IApplicationTargetGroup:
+#         return self._jobs_tg_2
--- src/stacks/base_stack.py
+++ src/stacks/base_stack.py
@@ -6,7 +6,6 @@ from custom_constructs.storage_construct import StorageConstruct
 from custom_constructs.iam_construct import IamConstruct
 from custom_constructs.alb_construct import AlbConstruct
 from custom_constructs.ecs_construct import EcsConstruct
-from custom_constructs.pipeline_construct import CodePipelineConstruct
 
 class BaseStack(cdk.Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
@@ -26,11 +25,10 @@ class BaseStack(cdk.Stack):
             self,
             "AlbConstruct",
             vpc=network.vpc,
-            security_group=network.alb_security_group,
-            subnets=network.vpc.public_subnets
+            security_group=network.alb_security_group
         )
 
-        # Create ECS resources (basic infrastructure only)
+        # Create ECS resources
         ecs = EcsConstruct(
             self,
             "EcsConstruct",
@@ -38,25 +36,25 @@ class BaseStack(cdk.Stack):
             security_groups=[network.service_security_group],
             execution_role=iam.task_execution_role,
             task_role=iam.task_role,
-            service_target_group=alb.service_tg_1,
-            jobs_target_group=alb.jobs_tg_1
+            service_target_group=alb.main_target_group,  # Changed from service_tg_1
+            jobs_target_group=alb.jobs_target_group     # Changed from jobs_tg_1
         )
 
         # Create Pipeline resources (handles deployments and updates)
-        pipeline = CodePipelineConstruct(
-            self,
-            "PipelineConstruct",
-            ecs_cluster=ecs.cluster,
-            ecs_service=ecs.service,
-            ecs_jobs_service=ecs.jobs_service,
-            prod_listener=alb.production_listener,
-            test_listener=alb.test_listener,
-            service_target_groups=[
-                alb.service_tg_1,
-                alb.service_tg_2
-            ],
-            jobs_target_groups=[
-                alb.jobs_tg_1,
-                alb.jobs_tg_2
-            ]
-        )
+        # pipeline = CodePipelineConstruct(
+        #     self,
+        #     "PipelineConstruct",
+        #     ecs_cluster=ecs.cluster,
+        #     ecs_service=ecs.service,
+        #     ecs_jobs_service=ecs.jobs_service,
+        #     prod_listener=alb.production_listener,
+        #     test_listener=alb.test_listener,
+        #     service_target_groups=[
+        #         alb.service_tg_1,
+        #         alb.service_tg_2
+        #     ],
+        #     jobs_target_groups=[
+        #         alb.jobs_tg_1,
+        #         alb.jobs_tg_2
+        #     ]
+        # )
