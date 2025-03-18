a00b59e2dd0525dcd751e705782e9188c1e89420
Dobson Dunavant <136488074+SavvasDobsonDunavant@users.noreply.github.com>
2025-03-18 00:03:10 -0600

    Update ecs_blue_green_stack.py

--- src/stacks/ecs_blue_green_stack.py
+++ src/stacks/ecs_blue_green_stack.py
@@ -4,7 +4,7 @@ from aws_cdk import (aws_ec2 as ec2)
 
 from custom_constructs.network_construct_new import NetworkConstruct
 from custom_constructs.ecr_construct_new import EcrConstruct
-from custom_constructs.load_balancer_construct_new import LoadBalancerConstruct
+from custom_constructs.alb_construct_new import AlbConstruct
 from custom_constructs.ecs_construct_new import EcsConstruct
 from custom_constructs.pipeline_construct_new import PipelineConstruct
 
