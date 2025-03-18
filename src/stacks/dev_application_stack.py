66724477780ca58c9bd2529185cfb5f55671f6f2
Dobson Dunavant <136488074+SavvasDobsonDunavant@users.noreply.github.com>
2025-03-18 15:35:41 -0600

    Update dev_application_stack.py

--- src/stacks/dev_application_stack.py
+++ src/stacks/dev_application_stack.py
@@ -24,7 +24,6 @@ class DevApplicationStack(cdk.Stack):
         network = NetworkConstruct(
             self,
             f"Network-{self.sub_environment}",
-            sub_environment=f"-{self.sub_environment}",
         )
 
         # ECR Repository
