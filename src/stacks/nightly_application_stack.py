6e505d2346467435c7364fdf3233f9649d173da0
Dobson Dunavant <136488074+SavvasDobsonDunavant@users.noreply.github.com>
2025-03-19 14:09:10 -0600

    rollback pipeline since outlier-api merge hasnt happened yet

--- src/stacks/nightly_application_stack.py
+++ src/stacks/nightly_application_stack.py
@@ -6,7 +6,7 @@ from custom_constructs.network_construct import NetworkConstruct
 from custom_constructs.ecr_construct import EcrConstruct
 from custom_constructs.alb_construct import AlbConstruct
 from custom_constructs.ecs_construct import EcsConstruct
-from custom_constructs.pipeline_construct import PipelineConstruct
+from custom_constructs.pipeline_construct_old import PipelineConstructOld
 from custom_constructs.waf_construct import WafConstruct
 from custom_constructs.storage_construct import StorageConstruct
 from custom_constructs.database_construct import DatabaseConstruct
@@ -70,7 +70,7 @@ class NightlyApplicationStack(cdk.Stack):
         )
 
         # CI/CD Pipeline
-        pipeline = PipelineConstruct(
+        pipeline = PipelineConstructOld(
             self,
             "Pipeline",
             service=ecs.service,
